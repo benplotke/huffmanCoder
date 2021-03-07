db25374b29b0cded93bb5ca3a84e867b16b95864
Ben Plotke <benplotke@gmail.com>
2021-03-07 12:22:54 -0800

    Update HuffmanCoder.java
    Added description directly to file

--- HuffmanCoder.java
+++ HuffmanCoder.java
@@ -8,6 +8,18 @@ import java.io.FileInputStream; //reads bytes from file
 import java.io.FileOutputStream; //writes bytes to file
 
 /**
+ * This is a java program which Huffman codes files on the byte level.
+ * Optimal compression is achieved on files with significant byte repitition, e.g text files.
+ * 
+ * This was made for an assignment intended to teach about trees. The initial assignment was
+ * focused around demonstration hence only required the huffman coding to be output as a
+ * String of ones and zeros. With the permission of the teacher I asked of I could take it
+ * one step further and output to file. In researching this project I relized examples of 
+ * java bit manipulation are somewhat rare on the internet; as such, I've decided to make
+ * this little bit of code freely available.
+ */
+
+/**
  * Can encode any file with huffman coding and decode files coded with this program
  * The encoding output has .bhc appended to the file name.
  * The interface is a command prompt.
